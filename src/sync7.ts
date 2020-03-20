6ab1feb0e3255ac0be8fdc27c58347217834b1eb
Chet Corcos <ccorcos@gmail.com>
2020-03-20 09:43:49 -0700

    More thinking

--- src/sync7.ts
+++ src/sync7.ts
@@ -4,63 +4,36 @@ https://gist.github.com/sid24rane/2b10b8f4b2f814bd0851d861d3515a10
 
 TODO:
 - test out that this thing even works.
+	- write out a test!
 - persist log to disk
-- create more than one sink.
-- abstract away the connection stuff from the log sync.
-- sync logs n-ways. chat app.
+	- create a simple CLI for this stuff.
+- sync logs n-ways.
+	- chat app. think about causality.
+
+Goal:
+- optionally synchronous and in-memory -- network entirely abstracted
 
 
 */
 
 import * as net from "net"
 
-const sourcePort = 8001
-const sinkPort = 8002
+const ports = [8001, 8002, 8003, 8004, 8005, 8006]
 
 export class Source {
-	log: Array<any> = []
-
-	constructor() {}
-
-	connections = new Set<net.Socket>()
-
-	// Server to accept connections from sinks when they come online.
-	startServer() {
-		var server = net.createServer()
-		server.on("connection", socket => {
-			this.connections.add(socket)
-			socket.setEncoding("utf8")
-			socket.on("data", data => {
-				const msg = JSON.parse(data.toString("utf8"))
-				this.onMessage(socket, msg)
-			})
-			socket.on("close", error => {
-				this.connections.delete(socket)
-			})
+	private connections: ConnnectionManager
+	constructor(private port: number) {
+		this.connections = new ConnnectionManager({
+			port,
+			ports,
+			onMessage: this.handleMessage,
+			onConnect: () => null,
 		})
-		server.listen(sourcePort)
 	}
 
-	// Clients to connect to sinks when coming online.
-	startClients() {
-		for (const port of [sinkPort]) {
-			var socket = new net.Socket()
-			socket.setEncoding("utf8")
-			socket.connect({ port })
-			socket.on("connect", () => {
-				this.connections.add(socket)
-			})
-			socket.on("data", data => {
-				const msg = JSON.parse(data.toString("utf8"))
-				this.onMessage(socket, msg)
-			})
-			socket.on("close", () => {
-				this.connections.delete(socket)
-			})
-		}
-	}
+	private log: Array<any> = []
 
-	onMessage(socket: net.Socket, msg) {
+	handleMessage = (socket: net.Socket, msg) => {
 		if (msg.type === "sync") {
 			if (msg.size < this.log.length) {
 				socket.write(
@@ -77,66 +50,24 @@ export class Source {
 	write(item: string) {
 		const index = this.log.length
 		this.log.push(item)
-		for (const connection of Array.from(this.connections)) {
-			connection.write(
-				JSON.stringify({ type: "update", index, values: [item] })
-			)
-		}
+		this.connections.broadcast({ type: "update", index, values: [item] })
 	}
 }
 
 export class Sink {
 	log: Array<any> = []
 
-	constructor() {}
-
-	connections = new Set<net.Socket>()
-
-	// Server to accept connections from source when they come online.
-	startServer() {
-		var server = net.createServer()
-		server.on("connection", socket => {
-			this.connections.add(socket)
-			socket.setEncoding("utf8")
-			socket.on("data", data => {
-				const msg = JSON.parse(data.toString("utf8"))
-				this.onMessage(socket, msg)
-			})
-			socket.on("close", error => {
-				this.connections.delete(socket)
-			})
-			// Initiate sync.
-			this.sync(socket)
+	private connections: ConnnectionManager
+	constructor(private port: number) {
+		this.connections = new ConnnectionManager({
+			port,
+			ports,
+			onMessage: this.handleMessage,
+			onConnect: this.handleConnect,
 		})
-		server.listen(sinkPort)
 	}
 
-	// Cleints to initiate connection to source when coming online
-	startClients() {
-		for (const port of [sourcePort]) {
-			var socket = new net.Socket()
-			socket.setEncoding("utf8")
-			socket.connect({ port })
-			socket.on("connect", () => {
-				this.connections.add(socket)
-				// Initiate sync.
-				this.sync(socket)
-			})
-			socket.on("data", data => {
-				const msg = JSON.parse(data.toString("utf8"))
-				this.onMessage(socket, msg)
-			})
-			socket.on("close", () => {
-				this.connections.delete(socket)
-			})
-		}
-	}
-
-	sync(socket: net.Socket) {
-		socket.write(JSON.stringify({ type: "sync", size: this.log.length }))
-	}
-
-	onMessage(socket: net.Socket, msg) {
+	handleMessage = (socket: net.Socket, msg) => {
 		if (msg.type === "update") {
 			if (msg.index > this.log.length) {
 				// Out of sync.
@@ -148,13 +79,85 @@ export class Sink {
 		}
 	}
 
-	write(item: string) {
-		const index = this.log.length
-		this.log.push(item)
-		for (const connection of Array.from(this.connections)) {
-			connection.write(
-				JSON.stringify({ type: "update", index, values: [item] })
-			)
+	handleConnect = (socket: net.Socket) => {
+		this.sync(socket)
+	}
+
+	sync(socket: net.Socket) {
+		socket.write(JSON.stringify({ type: "sync", size: this.log.length }))
+	}
+}
+
+export class ConnnectionManager {
+	private port: number
+	private ports: Array<number>
+	private onMessage: (socket: net.Socket, msg: any) => void
+	private onConnect: (socket: net.Socket) => void
+
+	constructor(args: {
+		port: number
+		ports: Array<number>
+		onMessage: (socket: net.Socket, msg: any) => void
+		onConnect: (socket: net.Socket) => void
+	}) {
+		this.port = args.port
+		this.ports = args.ports
+		this.onMessage = args.onMessage
+		this.onConnect = args.onConnect
+	}
+
+	private sockets = new Map<number, net.Socket>()
+
+	// Server to accept connections when others come online.
+	startServer() {
+		var server = net.createServer()
+		server.on("connection", socket => {
+			const port = socket.remotePort!
+			if (this.sockets.has(port)) {
+				console.warn("Client connection already exists!")
+			}
+			this.sockets.set(port, socket)
+
+			this.onConnect(socket)
+
+			socket.setEncoding("utf8")
+			socket.on("data", data => {
+				const msg = JSON.parse(data.toString("utf8"))
+				this.onMessage(socket, msg)
+			})
+			socket.on("close", error => {
+				this.sockets.delete(port)
+			})
+		})
+		server.listen(this.port)
+	}
+
+	// Clients to connect to sinks when coming online.
+	startClients() {
+		for (const port of this.ports) {
+			var socket = new net.Socket()
+			socket.setEncoding("utf8")
+			socket.connect({ port })
+			socket.on("connect", () => {
+				if (this.sockets.has(port)) {
+					console.warn("Server connection already exists!")
+				}
+				this.sockets.set(port, socket)
+				this.onConnect(socket)
+			})
+			socket.on("data", data => {
+				const msg = JSON.parse(data.toString("utf8"))
+				this.onMessage(socket, msg)
+			})
+			socket.on("close", () => {
+				this.sockets.delete(port)
+			})
+		}
+	}
+
+	broadcast(msg) {
+		for (const socket of Array.from(this.sockets.values())) {
+			socket.write(JSON.stringify(msg))
 		}
 	}
 }
